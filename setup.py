453d6461376b4d120657e2ef72dea6959b748d66
Peter Zaitcev / [USSX] Hares <USSX.Hares@yandex.ru>
2020-10-07 04:43:54 +0300

    Update setup.py
    Increment version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='license',
-    version='0.1a2',
+    version='0.1a3',
     description='Library that encapsulates free software licenses',
     long_description=''.join(open('README.rst').readlines()),
     keywords='license',
